debd16fbd3fb819380476a4eec87c1d7e5187472
Michael Hanson <michael.b.hanson@gmail.com>
2011-12-09 12:38:38 -0800

    Add tests for help and unknown arguments

--- sinatra-gen-test.sh
+++ sinatra-gen-test.sh
@@ -7,3 +7,19 @@ sinatra_gen='./sinatra-gen'
 it_displays_help_when_called_without_arguments() {
   $sinatra_gen | grep USAGE
 }
+
+it_displays_help_when_called_with_h() {
+  echo $($sinatra_gen -h) | grep USAGE
+}
+
+it_displays_help_when_called_with_help() {
+  echo $($sinatra_gen --help) | grep USAGE
+}
+
+it_fails_when_called_with_an_unknown_argument() {
+  echo $($sinatra_gen --he | head -n 3 ) | grep UNKNOWN
+}
+
+it_fails_when_called_with_an_unknown_argument_2() {
+  echo $($sinatra_gen -f | head -n 3 ) | grep UNKNOWN
+}
