d213e3992f9f86ed3ba10a43ed76519b508773b9
Michael Hanson <michael.b.hanson@gmail.com>
2011-12-09 22:12:40 -0800

    Add tests for creating directory

--- sinatra-gen-test.sh
+++ sinatra-gen-test.sh
@@ -25,3 +25,13 @@ it_displays_the_unknown_argument_in_the_error_message() {
   $sinatra_gen -f | grep "\-f"
   $sinatra_gen --he | grep "--he"
 }
+
+it_creates_a_directory_with_the_name_provided() {
+  $sinatra_gen foo
+  test -d foo
+}
+
+it_does_not_create_a_directory_if_it_already_exists() {
+  mkdir foo
+  $sinatra_gen foo | grep EXISTS
+}
