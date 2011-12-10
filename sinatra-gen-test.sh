74c65272a15b38cd3ab1c18cb37094a84aa47a79
Michael Hanson <michael.b.hanson@gmail.com>
2011-12-10 10:48:42 -0800

    Simplify content tests

--- sinatra-gen-test.sh
+++ sinatra-gen-test.sh
@@ -43,31 +43,25 @@ it_does_not_create_a_directory_if_it_already_exists() {
 it_creates_an_app_dot_rb_file() {
   $sinatra_gen foo
   test -f foo/app.rb
-  grep Bundler foo/app.rb
-  grep AssetPack foo/app.rb
-  grep mustache foo/app.rb
+  grep Sinatra::Base foo/app.rb
 }
 
 it_creates_a_gemfile() {
   $sinatra_gen foo
   test -f foo/Gemfile
-  grep sinatra foo/Gemfile
-  grep mustache foo/Gemfile
-  grep end foo/Gemfile
+  grep sinatra/base foo/Gemfile
 }
 
 it_creates_a_config_ru_file() {
   $sinatra_gen foo
   test -f foo/config.ru
   grep app.rb foo/config.ru
-  grep App foo/config.ru
 }
 
 it_creates_a_layout_view() {
   $sinatra_gen foo
   file=foo/views/layout.rb
   test -f $file
-  grep Layout $file
   grep @title $file
 }
 
@@ -75,8 +69,6 @@ it_creates_a_layout_template() {
   $sinatra_gen foo
   file=foo/templates/layout.mustache
   test -f $file
-  grep doctype $file
-  grep title $file
   grep yield $file
 }
 
@@ -85,7 +77,6 @@ it_creates_an_index_view() {
   file=foo/views/index.rb
   test -f $file
   grep Index $file
-  grep end $file
 }
 
 it_creates_an_index_template() {
